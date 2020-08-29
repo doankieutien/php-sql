bf63c37d09eabcabbdcc10229db464b96e32676e
doankieutien <69305542+doankieutien@users.noreply.github.com>
2020-08-29 19:09:39 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
-echo "<h1>Xin chào php <h1>";
-?>
+     include_once(__DIR__ .'/../layouts/styles.php');
+    
+    ?>
+    <?php
+     include_once(__DIR__ .'/../layouts/partials/header.php');
+    
+    ?>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-2">
+        <?php
+    include_once(__DIR__ .'/../layouts/partials/sidebar.php');
+    ?>
+        </div>
+        <div class="col-md-8">
+        <button name="create" value="Tao moi"></button>
+        <h1>Bang hinh thuc thanh toan</h1>
+        <?php
+        include_once(__DIR__ . '/../../example/bang_xoa.php');
+        ?>
+        </div>
+    </div>
+    
+    </div>
+    
+    <?php
+    include_once(__DIR__ .'/../layouts/partials/sidebar.php');
+    ?>
+    <?php
+    include_once(__DIR__ .'/../layouts/partials/footer.php');
+    ?>
+    <?php
+    include_once(__DIR__ .'/../layouts/scripts.php');
+    ?>
+
+</body>
+</html>
